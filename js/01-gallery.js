7534d82722b5d1c5364df9b61facc7b60e13be87
HelenLebed <110856304+HelenLebed@users.noreply.github.com>
2022-12-13 00:04:59 +0200

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// query div from html
+const gallery = document.querySelector(".gallery");
+
+// iterated array of objects, created a markup according to the template,joined to a string
+const htmlString = galleryItems
+    .map(
+        (obj) => `<div class="gallery__item">
+<a class="gallery__link" href=${obj.original}>
+  <img
+    class="gallery__image"
+    src=${obj.preview}
+    data-source=${obj.original}
+    alt=${obj.description}
+  />
+</a>
+</div>`
+    )
+    .join("");
+
+// added markup to div
+gallery.innerHTML = htmlString;
+
+//created EventListener for div
+gallery.addEventListener("click", handleClick);
+
+// created cb function
+function handleClick(event) {
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    event.preventDefault();
+    const original = event.target.dataset.source;
+    const instance = basicLightbox.create(` <img
+    src=${original}
+    />`);
+    instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const htmlString = galleryItems
+    .map(
+        (obj) => `
+<a class="gallery__item" href=${obj.original}>
+  <img
+    class="gallery__image"
+    src=${obj.preview}
+    alt=${obj.description}
+  />
+</a>`
+    )
+    .join("");
+
+gallery.innerHTML = htmlString;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionPosition: `bottom`,
+    captionsData: `alt`,
+    captionDelay: 250,
+});
